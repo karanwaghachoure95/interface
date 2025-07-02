ebf754ab153df79a1dc793914fa361b97f40d529
karanwaghachoure <karanwaghachoure@gmail.com>
2025-07-02 21:31:11 +0530

    Interface program

--- src/com/Interface/Calculator.java
+++ src/com/Interface/Calculator.java
@@ -2,8 +2,6 @@ package com.Interface;
 
 public interface Calculator {
  abstract void add(int a,int b);
- abstract void sub(int a,int b);
- abstract void mul(int a,int b);
- abstract void div(int a,int b);
+
 
 }
